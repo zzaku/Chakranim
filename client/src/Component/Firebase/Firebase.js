85a4ac43e3b6556ace5872b4ad8d71ee77b50c07
zzaku <enzo.makked@gmail.com>
2022-08-15 03:44:50 +0200

    firebase fixed

--- client/src/Component/Firebase/Firebase.js
+++ client/src/Component/Firebase/Firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore"
 import 'firebase/auth'
 
 const app =  initializeApp({
-  apiKey: "AIzaSyDI8wtsp3-QZVtOsfoNnAphtSaOFqGWR2Y",
-  authDomain: "chakranimes-67c42.firebaseapp.com",
-  projectId: "chakranimes-67c42",
-  storageBucket: "chakranimes-67c42.appspot.com",
-  messagingSenderId: "741902551118",
-  appId: "1:741902551118:web:1a78ea4edd1d85a2b69b1f",
+  apiKey: "AIzaSyAY6b-unMpwJF34TZTaUlKnthJOFHkW-y8",
+  authDomain: "chakranimes-9dec8.firebaseapp.com",
+  projectId: "chakranimes-9dec8",
+  storageBucket: "chakranimes-9dec8.appspot.com",
+  messagingSenderId: "906490506414",
+  appId: "1:906490506414:web:357bdf99b1d3ada045513f"
   });
 
 export const auth = getAuth(app);
