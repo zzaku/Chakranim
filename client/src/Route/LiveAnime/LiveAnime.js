e145e721aea1a24a599c0ae4678fd1908d5b488a
zzaku <enzo.makked@gmail.com>
2022-09-03 20:05:49 +0200

    styled component removed

--- client/src/Route/LiveAnime/LiveAnime.js
+++ client/src/Route/LiveAnime/LiveAnime.js
@@ -2,19 +2,10 @@ import { Button } from "@mui/material"
 import { useState } from "react"
 import StreamAnime from "./ComponentsLive/StreamAnime/StreamAnime"
 import { SocketProvider, } from "../../Component/Context/SocketContext"
-import styled from "styled-components"
 import "./style/LiveAnime.css"
 
 
 const LiveAnime = () => {
-    
-    const ManageLive = styled("div")`
-    display: flex;
-    height: 100vh;
-    width: 100vw;
-    justify-content: center;
-    align-items: center;
-    `;
 
     const [goToPlayerVOD, setGoToPlayerVOD] = useState(false);
     const [open, setOpen] = useState(false)
@@ -22,7 +13,7 @@ const LiveAnime = () => {
 
     return (
         <SocketProvider>
-            <ManageLive>
+            <div className="manage-live-container">
                 <div className="open-live-vod" style={{height: goToPlayerVOD ? ("100%")  : (open ? "80%" : "50px"), width: goToPlayerVOD ? ("100%")  : (open ? "80%" : "220px"), border: goToPlayerVOD ? ("0")  : "25px"}}>
                     {open ?
                     <StreamAnime goToPlayerVOD={goToPlayerVOD} setGoToPlayerVOD={setGoToPlayerVOD} />
@@ -30,7 +21,7 @@ const LiveAnime = () => {
                     <Button onClick={() => setOpen(true)} style={{display: "flex", borderRadius: "25px", color: "white", height: "auto", width: "100%", justifyContent: "center", alignItems: "center"}}>OK</Button>
                     }
                 </div>
-            </ManageLive>
+            </div>
         </SocketProvider>
 
     )
